706d5a3b004fe02788886913e9df05e08d9e2e5e
Rachael Passov <rachael.passov@vertical-knowledge.com>
2016-02-02 00:32:23 -0500

    moved dotfiles, symlinking now, git script moved to historical and needs to be rewritten

---
+++ bin/install_dotfiles.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in $HOME/dotfiles
+# Derived from https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
+############################
+
+########## Variables
+
+source_repo="$HOME/Projects/dotfiles"   # git repository where dotfiles are kept (can be same as $dotfiles_dir)
+dotfiles_dir="$HOME/.dotfiles"          # dotfiles directory
+backup_dir="$HOME/.dotfiles.bak"        # old dotfiles backup directory
+
+# list of paths to symlink in $HOME (can be longer path)
+# target can be a space-separated list of directories
+declare -A symlinks=(
+    [".bash_profile"]=".bash_profile .bashrc"
+    [".vimrc"]=".vimrc"
+    ["bin"]="bin"
+)
+
+# for nice output
+LIGHTGRAY='\e[0;37m'
+WHITE='\e[1;37m'
+
+##########
+
+# runs command in a subshell & catches any errors
+# prints "done" or "failed" first, then prints the errors
+# useful after an echo -n
+# ex:   $ echo -n "Performing task ..."; subsh_and_delay_err task_that_will_fail;
+#       $ Performing task ...failed
+#       $ error is printed here
+function subsh_and_delay_err () {
+    err=$( ($@) 2>&1 ) && echo "done" || echo "failed"
+    if [ "$err" ]; then echo "$err"; fi
+}
+
+# if needed, symlink source repository to $dotfiles_dir
+# otherwise skip and don't spam the terminal w/unneccessary output
+if [[ ! "${source_repo}" == "${dotfiles_dir}" && ! "$(readlink ${dotfiles_dir})" == "$source_repo" ]]; then
+    echo -n "${LIGHTGRAY}Linking ${WHITE}$source_repo ${LIGHTGRAY}-> ${WHITE}$dotfiles_dir ${LIGHTGRAY}..."
+    subsh_and_delay_err ln -shf $source_repo $dotfiles_dir
+fi
+
+# if needed, create $backup_dir in $HOME
+# otherwise skip and don't spam the terminal w/unneccessary output
+if [ ! -e $backup_dir ]; then
+    echo -n "${LIGHTGRAY}Creating ${WHITE}$backup_dir ${LIGHTGRAY}for backup of any existing dotfiles in $HOME ..."
+    subsh_and_delay_err mkdir -p $backup_dir
+fi
+
+# move any existing dotfiles in $HOME to $backup_dir
+# create symlinks from $HOME to any files in $dotfiles_dir specified in $symlinks
+for path in ${!symlinks[@]}; do
+    for target in ${symlinks["$path"]}; do
+        if [[ -e "$HOME/$target" && ! -L "$HOME/$target" ]]; then
+            echo -n "${LIGHTGRAY}Backing up ${WHITE}"$target" ${LIGHTGRAY}..."
+            subsh_and_delay_err mv "$HOME/$target" "$backup_dir/"
+        fi
+        if [[ ! "$(readlink "${dotfiles_dir}/${path}")" == "$HOME/$target" ]]; then
+            printf "${LIGHTGRAY}Linking ${WHITE}$target ${LIGHTGRAY}-> ${WHITE}${dotfiles_dir#$HOME/}/$path ${LIGHTGRAY}in $HOME ..."
+            subsh_and_delay_err ln -shf $dotfiles_dir/$path $HOME/$target
+        fi
+    done
+done
+
+printf "${ENDCOLOR}"
